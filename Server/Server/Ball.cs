c18b2198dae1b867634d1f90d8f8bfe37c7afac5
Sergey <71557182+ewqdsa132@users.noreply.github.com>
2022-11-18 16:38:11 +0300

    Update Ball.cs

--- Server/Server/Ball.cs
+++ Server/Server/Ball.cs
@@ -2,10 +2,8 @@ namespace Server
 {
     public class Ball
     {
-        public void Initialize()
-        {
-        }
-
+        public void Initialize() { }
+        
         public void ServerSideUpdate(GameTime gameTime)
         {
             float timeDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
